4e13cc48c7ddfa95cb96122be2ec6bf993b4d63b
mk47q <mtynkarjw@gmail.com>
2024-03-04 00:59:26 +0330

    feat: add Error 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import NotFound from '@/views/Error404.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     {
       path: '/',
       name: 'home',
